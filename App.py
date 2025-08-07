b2498f0aa7b69a679c4b23767f7e8711a8d46f69
CreativePatterns <mohammed.radha.alsaeedi@gmail.com>
2025-08-07 04:39:31 +0300

    Ver. 0.1

--- App.py
+++ App.py
@@ -1,13 +1,33 @@
 import Generator
-
+from Generator import mood_map
 
 """ Type a mood and get a randomly generated character!!! """
+keys = []
 
-mood = input("Type a mood to generate a character: ")
+# The app starts here
+def start_app():
 
-Generator.make_character(mood)
+    # show available options
+    for key in mood_map.keys():
+        keys.append(key)
+    print(keys)
+    mood = input("Type a mood to generate a character: ")
+
+    if mood not in Generator.mood_map:
+        print("Invalid mood")
+        exit()
+
+    print(Generator.MODES)
+    mode = input("Please choose a mode: ")
+    mode = mode.capitalize()
+
+    if mode not in Generator.MODES:
+        print("Please choose a valid mode")
+        exit()
+
+    Generator.make_character(mood, mode)
 
 
+start_app()
 
-
-
+# I want the user to be able to toggle between 2 different modes (single and dual characters)
--- Generator.py
+++ Generator.py
@@ -2,71 +2,147 @@ from json import load
 import random
 
 OUTPUT_FILE = "npc_card.txt"
+MODES = ["Single" , "Duo"]
 
-names = load(open('names.json'))
-classes = load(open('classes.json'))
-realms = load(open('realms.json'))
-traits = load(open('traits.json'))
-mood_map = load(open('mood map.json'))
-backstory = load(open('backstory.json'))
+names = load(open('data_bank/names.json'))
+classes = load(open('data_bank/classes.json'))
+realms = load(open('data_bank/realms.json'))
+traits = load(open('data_bank/traits.json'))
+mood_map = load(open('data_bank/mood map.json'))
+backstory = load(open('data_bank/backstory.json'))
+action = load(open('data_bank/action.json'))
+event = load(open('data_bank/event.json'))
+duo_backstory = load(open('data_bank/duo_backstory.json'))
+side_quest = load(open('data_bank/side_quest.json'))
+quest_info = load(open('data_bank/quest_info.json'))
+
+weird_location = random.choice(quest_info['weird_locations'])
+weird_objects = random.choice(quest_info['weird_objects'])
+faction =  random.choice(quest_info['factions'])
 
 
-def make_character(mood):
-    check_user_mood(mood)
+def make_character(mood, mode):
 
-def check_user_mood(mood):
-    # Make sure only the moods in the mood map are entered
-    if mood not in mood_map:
-        print("Mood not found")
-        exit()
-    generate_random_story(mood)
+    # Check which mode the user chose!
+    if mode == MODES[0]:
+        generate_single_character(mood, mode)
+    if mode == MODES[1]:
+        generate_duo_character(mood, mode)
 
-
-def generate_random_story(mood):
+def generate_single_character(mood, mode):
     character_name = random.choice(names)
     character_work = random.choice(classes)
     character_realm = random.choice(realms)
     character_trait = random.choice(traits)
+    character_event = random.choice(event)
+    character_action = random.choice(action)
     character_backstory = random.choice(backstory[mood])
+    character_side_quest = random.choice(side_quest[mood])
     char_mood = random.choice(mood_map[mood])
 
-    # TODO
-    # add event and action later, make the things that you have usable
-    generate_backstory(character_backstory, character_name, character_realm, character_work, character_trait)
+    generate_backstory(character_backstory, character_name, character_realm, character_work
+                       , character_trait, character_event, character_action, mode , character_side_quest, char_mood)
+
+def generate_duo_character(mood, mode):
+    first_char = random.choice(names)
+    second_char = random.choice(names)
+    first_mood = random.choice(mood_map[mood])
+    second_mood = random.choice(mood_map[mood])
+
+    character_side_quest = random.choice(side_quest[mood])
+
+    # make sure there aren't any duplicate characters or moods
+    while first_char == second_char:
+        second_char = random.choice(names)
+
+    while first_mood == second_mood:
+        second_mood = random.choice(mood_map[mood])
+
+    character_realm = random.choice(realms)
+    character_event = random.choice(event)
+    character_action = random.choice(action)
+    chosen_duo_backstory = random.choice(duo_backstory[mood])
+
+    generate_duo_backstory(first_char, second_char, chosen_duo_backstory,
+                           character_event, character_action, character_realm, mode, character_side_quest, first_mood, second_mood)
 
 
-def generate_backstory(character_backstory, character_name, character_realm, character_work, character_trait,
-                       event="Judgement day", action="mopping the floor"):
+def generate_backstory(character_backstory, character_name, character_realm,
+                       character_work, character_trait,character_event, character_action, mode, character_side_quest, char_mood):
 
-    finished_character_backstory = character_backstory.format(name=character_name, event=event,
-                                                              realm=character_realm, action=action)
+    finished_character_backstory = character_backstory.format(name=character_name, event=character_event,
+                                                              realm=character_realm, action=character_action)
 
-    final_character = {
+
+    finished_side_quest = character_side_quest.format(object=weird_objects, weird_location=weird_location, faction=faction)
+    character_sheet = {
         "name": character_name,
+        "mood": char_mood,
         "event": event,
         "action": action,
         "character_work": character_work,
         "character_trait": character_trait,
         "character_backstory": finished_character_backstory,
         "character_realm": character_realm,
+        "side_quest": finished_side_quest,
     }
-
-    output_and_save_character(final_character, OUTPUT_FILE)
-
+    output_and_save_character(character_sheet, OUTPUT_FILE, mode)
 
 
-def output_and_save_character(character, file_name):
-    # Print full character
-    print(f"\n🧙 Name: {character['name']}")
-    print(f"💼 Class: {character['character_work']}")
-    print(f"📍 Realm: {character['character_realm']}")
-    print(f"🧠 Trait: {character['character_trait']}")
-    print(f"📜 Backstory:\n{character['character_backstory']}")
+def generate_duo_backstory(first_char, second_char, chosen_duo_backstory,
+                           character_event, character_action, character_realm, mode, character_side_quest, first_mood, second_mood):
 
-    with open(file_name, "w", encoding="utf-8") as f:
+    finished_duo_backstory = chosen_duo_backstory.format(name1=first_char,name2=second_char, event=character_event,
+                                                              realm=character_realm, action=character_action)
+
+    finished_side_quest = character_side_quest.format(object=weird_objects, weird_location=weird_location, faction=faction)
+
+    character_sheet = {
+        "Name1": first_char,
+        "Mood1": first_mood,
+        "Name2": second_char,
+        "Mood2": second_mood,
+        "Duo Backstory": finished_duo_backstory,
+        "side_quest": finished_side_quest,
+    }
+    output_and_save_character(character_sheet, OUTPUT_FILE, mode)
+
+
+def output_and_save_character(character, file_name, mode):
+
+    if mode == "Single":
         # Print full character
-        f.write(f"\n🧙 Name: {character['name']}\n")
-        f.write(f"💼 Class: {character['character_work']}\n")
-        f.write(f"📍 Realm: {character['character_realm']}\n")
-        f.write(f"🧠 Trait: {character['character_trait']}\n")
-        f.write(f"📜 Backstory:\n{character['character_backstory']}\n")
+        print(f"\n🧙 Name: {character['name']}")
+        print(f" They feel: {character['mood']}")
+        print(f"💼 Class: {character['character_work']}")
+        print(f"📍 Realm: {character['character_realm']}")
+        print(f"🧠 Trait: {character['character_trait']}")
+        print(f"📜 Backstory:\n{character['character_backstory']}\n")
+        print(f"Side Quest:\n{character['side_quest']}")
+
+        with open(file_name, "w", encoding="utf-8") as f:
+            # Print full character
+            f.write(f"\n🧙 Name: {character['name']}\n")
+            f.write(f"They feel: {character['mood']}\n")
+            f.write(f"💼 Class: {character['character_work']}\n")
+            f.write(f"📍 Realm: {character['character_realm']}\n")
+            f.write(f"🧠 Trait: {character['character_trait']}\n")
+            f.write(f"📜 Backstory:\n{character['character_backstory']}\n")
+            f.write(f"Side Quest:\n{character['side_quest']}")
+
+    elif mode == "Duo":
+        print(f"🧙 Name: {character["Name1"]}")
+        print(f" They feel: {character['Mood1']}")
+        print(f"🧙 Name: {character["Name2"]}")
+        print(f" They feel: {character['Mood2']}")
+        print(f"📜 Backstory:\n{character['Duo Backstory']}\n")
+        print(f"Side Quest:\n{character['side_quest']}")
+
+        with open(file_name, "w", encoding="utf-8") as f:
+            # Print full character
+            f.write(f"🧙 Name: {character["Name1"]}\n")
+            f.write(f"They feel: {character['Mood1']}\n")
+            f.write(f"🧙 Name: {character["Name2"]}\n")
+            f.write(f"They feel: {character['Mood2']}\n")
+            f.write(f"📜 Backstory:\n{character["Duo Backstory"]}\n")
+            f.write(f"Side Quest:\n{character['side_quest']}")
